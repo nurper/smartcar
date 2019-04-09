434812a5ec53bacb721be1f3c709008c8347aa94
nurper <nurken_99@mail.ru>
2019-04-09 21:04:01 +0600

    add leds and security, ignition variables

--- proje.ino
+++ proje.ino
@@ -2,7 +2,11 @@
 #define echoPin 3
 int lightSensor = A0;
 int lightValue = 0;
-int led1 = 1;
+int security = HIGH; //turns off when opened with 'key'
+int ignition = LOW; //tunrns on when pressed ignition button
+int frontled1 = 4;
+int frontled2 = 5;
+int securityled = 6;//blinks when burglar ignites the car
 void setup() {
   // put your setup code here, to run once:
 pinMode(trigPin, OUTPUT); // setting the output pin with fast output (will output signals in high speed)
@@ -12,7 +16,14 @@ pinMode(echoPin, INPUT); // setting the input pin with fast input (will get sign
 void loop() {
   // put your main code here, to run repeatedly:
   lightValue = analogRead(lightSensor);
-  if(lightValue<135){
-    led1 = HIGH;
+
+  if(ignition==HIGH){
+    if(lightValue<135){
+      frontled1 = HIGH;
+      frontled2 = HIGH;
+    }else{
+      frontled1 = LOW;
+      frontled2 = LOW;
+    }
   }
 }
